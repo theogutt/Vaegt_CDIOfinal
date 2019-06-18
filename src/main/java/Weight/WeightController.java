1e5671b7ebbd00299dbaef08c4ab4d2f04533cb1
Andreas <mail.Tailspot@gmail.com>
2019-06-18 14:44:13 +0200

    Fixed crash ved tomt input

--- src/main/java/Weight/WeightController.java
+++ src/main/java/Weight/WeightController.java
@@ -184,7 +184,7 @@ public class WeightController {
     }
 
     private int inputToInt(String input) {
-        if(input.equals("")){return -1;}
+        if(input.equals("RM20A\"\"")){return -1;}
         if(input.equals("RM20C")){return -1;}
         int num;
         input = input.replace(" ", "");
